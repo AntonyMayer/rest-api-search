5a33e24bcf77ec2f47b3e3e0ee06fa5e00eca552
Anton Kuzniatsou <anton.kuzniatsou@isobar.com>
2018-10-31 13:33:54 -0500

    Scss added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App__header">
-          Header
+          Search UI
         </header>
       </div>
     );
