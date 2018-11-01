f8f65fb76c15a728c1c7fdc83f6ccc398ba56bba
Anton Kuzniatsou <anton.kuzniatsou@isobar.com>
2018-11-01 18:07:41 -0500

    Matches implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Table from './components/Table';
 import Search from './components/Search';
+import Matches from './components/Matches';
+import Table from './components/Table';
 
 const App = () => (
 	<div className="App">
 		<h1 className="App__title">Search</h1>
 		<Search />
+		<Matches />
 		<Table />
 	</div>
 );
