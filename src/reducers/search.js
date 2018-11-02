eac60d732bc87b8a13989c6a61f6b5857f07a94a
Anton Kuzniatsou <anton.kuzniatsou@isobar.com>
2018-11-02 06:33:32 -0500

    Cleaning

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,16 +1,10 @@
-export default function data(state = { query: null, received: {} }, action) {
+export default function data(state = { query: null, received: null }, action) {
 	switch (action.type) {
 		case 'FETCH_DATA': 
 			return {
 				...state,
 				query: action.payload.query,
 				received: action.payload.results,
-			};
-		case 'RESET_QUERY':
-			return {
-				...state,
-				query: null,
-				received: null,
 			};
 		default: 
 			return state;
