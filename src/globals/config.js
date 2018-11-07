1ccbead619a469ded12161255530a6845f4fc354
Anton Kuzniatsou <anton.kuzniatsou@isobar.com>
2018-11-07 15:16:57 -0600

    Empty config added

---
+++ src/globals/config.js
@@ -0,0 +1,22 @@
+// Should not be a part of code, using it to avoid creating login screen
+const MASTER = {
+    username: '',
+    password: '',
+};
+
+/**
+ * API Endpoints
+ */
+const AUTH_ENDPOINT = '';
+const SEARCH_ENDPOINT = '';
+
+// Should be a dynamic value set by user
+// but for the sake of saving time using a hardcoded value
+const RESULTS_PER_PAGE = 25;
+
+export {
+    MASTER,
+    SEARCH_ENDPOINT,
+    AUTH_ENDPOINT,
+    RESULTS_PER_PAGE,
+};
